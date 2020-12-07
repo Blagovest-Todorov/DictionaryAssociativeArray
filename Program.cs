50de5fed8f68b1c748ef570fb75e85ce8194dfe7
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-07 13:23:29 +0200

    more exersize

--- Program.cs
+++ Program.cs
@@ -12,11 +12,17 @@ namespace _15.AssociativeArray
             phonebook["George"] = "1234564789";
             phonebook["Peter"] = "98745612";
             phonebook["Aleks"] = "98745613";
+            phonebook.Remove("George"); //@ We remove teh key-Value pair before we add it again!
+            //  phonebook.Clear(); //clears all  content, result is defaut value 0
+            phonebook.Add("George", "Hello");
+
             Console.WriteLine(phonebook.Count); //give me the value of teh key named -> George
             Console.WriteLine(String.Join(',', phonebook.Keys)); //printes collection of keys
             Console.WriteLine(String.Join(',', phonebook.Values)); //printes a collection of values
             Console.WriteLine(String.Join(',', phonebook)); //printes the pairs :  Key->Value;
-            // key-Value Entry -key-value record-> key value pair;
+                                                            // key-Value Entry -key-value record-> key value pair;
+
+
 
         }
     }
